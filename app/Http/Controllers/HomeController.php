b2b444c886034ba305c36afa23e7db90b73935bf
Matthe <m_jacobs@hotmail.nl>
2021-05-26 01:54:01 +0200

    First Commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //Redirects to login screen if user is not logged in
+        //$this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('pages/home');
+    }
+}
---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,8 @@
+<title>PlanMe @yield('title')</title>
+<link rel="stylesheet" href="/css/app.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+<link rel="stylesheet" href="/css/custom.css">
+<!-- Er kan een helper gebruikt worden voor het invoegen van een stylesheet <link rel="stylesheet" href="/css/custom.css">-->
+{{ Html::style("/css/custom.css") }}
+
+@yield('stylesheets')
